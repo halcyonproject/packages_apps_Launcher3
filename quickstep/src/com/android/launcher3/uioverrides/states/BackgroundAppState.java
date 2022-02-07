59899cebfc04b54fc2fbea529884d1e833f3165d
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    Launcher3: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    iusmac: adapt to A13.
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    [neobuddy89: adapt to A13 QPR2, A14 QPR2, A15, A16]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- quickstep/src/com/android/launcher3/uioverrides/states/BackgroundAppState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/BackgroundAppState.java
@@ -77,6 +77,7 @@ public class BackgroundAppState extends OverviewState {
         return super.getVisibleElements(launcher)
                 & ~OVERVIEW_ACTIONS
                 & ~CLEAR_ALL_BUTTON
+                & ~MEMINFO
                 & ~VERTICAL_SWIPE_INDICATOR
                 & ~ADD_DESK_BUTTON;
     }
