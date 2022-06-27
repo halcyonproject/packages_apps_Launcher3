b989336b90bcfe1600761c41a678e69fb5db8023
Pranav Vashi <neobuddy89@gmail.com>
2022-06-28 02:03:17 +0530

    Launcher3: Add pref to disable wallpaper scrolling
    Ref:
    https://github.com/LawnchairLauncher/lawnchair/commit/a969df9d3877aedcbfc0c5cdedc4b93b9b78048d
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -243,6 +243,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField
         val ENABLE_TWOLINE_ALLAPPS_TOGGLE = backedUpItem("pref_enable_two_line_toggle", false)
         @JvmField val PROMISE_ICON_IDS = backedUpItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
+        @JvmField val WALLPAPER_SCROLLING = backedUpItem("pref_allow_wallpaper_scrolling", true)
         @JvmField val WORK_EDU_STEP = backedUpItem("showed_work_profile_edu", 0)
         @JvmField
         val WORKSPACE_SIZE =
