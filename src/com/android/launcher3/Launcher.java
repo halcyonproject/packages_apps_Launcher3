c475d5ea45f54eb8f8e7865e183ca17026334ff3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-08 23:34:07 +0100

    Launcher3: Improve restart logic
    - Use main executor
    - Use System.exit() instead of killProcess()
    - Reduce wait period
    
    Change-Id: Ibc27b412153110eada13b850b7dfcf32d00a0f2b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1336,7 +1336,7 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         if (mNeedsRestart) {
             Toast.makeText(this, R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
-            Utilities.restart(this);
+            Utilities.restart();
         }
     }
 
